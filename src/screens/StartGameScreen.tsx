421037a0ba1d588e1996c12ed70c036068dde6e4
hudaMohamed <huda.abdallah@vodafone.com>
2024-05-01 18:15:42 +0300

    intiaize project

---
+++ src/screens/StartGameScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <Text>StartGameScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+export default StartGameScreen
